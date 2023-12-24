a6d541235c24946517999a20bc77197de2bbf12e
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2023-12-24 01:36:00 -0800

    Started work on profile page

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProfileLeft from './ProfileLeft/ProfileLeft'
+import './Profile.css'
+
+const Profile = () => {
+    return (
+        <div className="Profile">
+            <ProfileLeft/>
+        </div>
+    )
+}
+
+export default Profile
---
+++ src/pages/Profile/ProfileLeft/InfoCard/InfoCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './InfoCard.css'
+import {UilPen} from '@iconscout/react-unicons'
+
+const InfoCard = () => {
+    return (
+        <div className="InfoCar">
+            <div className="InfoHead">
+                <h4>Your Info</h4>
+                <UilPen/>
+
+            </div>
+        </div>
+    )
+}
+
+export default InfoCard
