976edd8d29c12c1629f827e2b79b606878b9a79b
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-01-01 21:22:47 -0800

    Added the ability to delete a post

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -29,5 +29,50 @@ export const getPost = async(req, res)=> {
     }
 }
 
+// Update a Post
+
+export const updatePost = async(req, res)=>{
+    const postId = req.params.id
+    const {userId} = req.body
+
+    try {
+        const post = await PostModel.findById(postId)
+        if(post.userId === userId)
+        {
+            await post.updateOne( { $set : req.body})
+            res.status(200).json("Post Updated")
+        }
+        else{
+            res.status(403).json("Action forbidden")
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 
 
+// Delete a Post
+
+export const deletePost = async (req, res)=> {
+    const id = req.params.id;
+    const{userId} = req.body
+
+    try {
+
+        const post = await PostModel.findById(id)
+        if(post.userId === userId)
+        {
+            await post.deleteOne();
+            res.status(200).json("Post deleted successfully")
+
+        }
+        else{
+            res.status(403).json("Action forbidden")
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+}
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost, getPost } from "../Controllers/PostController.js";
+import { createPost, getPost, updatePost, deletePost } from "../Controllers/PostController.js";
 const router = express.Router()
 
 router.post('/', createPost)
 router.get('/:id', getPost)
-
+router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
 export default router;
 
