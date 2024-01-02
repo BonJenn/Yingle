b684ad5bd15d41ccda5a0147b45adbe7db151cad
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-01-01 22:19:09 -0800

    Added the ability to like/unlike posts

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -76,3 +76,23 @@ export const deletePost = async (req, res)=> {
     }
 
 }
+
+// Like and Disklike a Post
+
+export const likePost = async (req, res )=> {
+    const id = req.params.id
+    const {userId} = req.body
+
+    try {
+        const post = await PostModel.findById(id)
+        if(!post.likes.includes(userId)) {
+            await post.updateOne({$push : {likes : userId}})
+            res.status(200).json("Post liked")
+        } else {
+            await post.updateOne({$pull : {likes : userId}})
+            res.status(200).json("Post unliked")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
